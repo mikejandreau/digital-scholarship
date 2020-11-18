b74b08c870557b575f8f128b8855a6fe01d8067f
Mike Jandreau <mikejandreau@gmail.com>
2020-11-18 15:17:49 -0500

    Added function to hide "Protected" from titles

--- functions.php
+++ functions.php
@@ -407,4 +407,11 @@ function digitalscholarship_numeric_posts_nav() {
 	echo '</ul></div>' . "\n";
 }
 
-
+/**
+* Removes or edits the 'Protected:' part from posts titles
+*/
+ 
+add_filter( 'protected_title_format', 'remove_protected_text' );
+function remove_protected_text() {
+return __('%s');
+}
